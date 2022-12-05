4a050258319592596e486aaad4e83176505b228f
h <angelinacom157@gmail.com>
2022-12-05 23:35:46 +0300

    Initial commit

---
+++ src/DataSource.java
@@ -0,0 +1,5 @@
+public interface DataSource {
+    void writeData(String data);
+
+    String readData();
+}
---
+++ src/FileDataSource.java
@@ -0,0 +1,83 @@
+import javax.sql.DataSource;
+import java.io.*;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.logging.Logger;
+
+public class FileDataSource implements DataSource {
+    private String name;
+
+    public FileDataSource(String name) {
+        this.name = name;
+    }
+
+
+    public void writeData(String data) {
+        File file = new File(name);
+        try (OutputStream fos = new FileOutputStream(file)) {
+            fos.write(data.getBytes(), 0, data.length());
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+
+    public String readData() {
+        char[] buffer = null;
+        File file = new File(name);
+        try (FileReader reader = new FileReader(file)) {
+            buffer = new char[(int) file.length()];
+            reader.read(buffer);
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return new String(buffer);
+    }
+
+
+    @Override
+    public Connection getConnection() throws SQLException {
+        return null;
+    }
+
+    @Override
+    public Connection getConnection(String username, String password) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public PrintWriter getLogWriter() throws SQLException {
+        return null;
+    }
+
+    @Override
+    public void setLogWriter(PrintWriter out) throws SQLException {
+
+    }
+
+    @Override
+    public void setLoginTimeout(int seconds) throws SQLException {
+
+    }
+
+    @Override
+    public int getLoginTimeout() throws SQLException {
+        return 0;
+    }
+
+    @Override
+    public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+        return null;
+    }
+
+    @Override
+    public <T> T unwrap(Class<T> iface) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public boolean isWrapperFor(Class<?> iface) throws SQLException {
+        return false;
+    }
+}
